2a01e7e9511443faf33efa599ff19621aaef36a0
chuyejinn <chuyejin0403@naver.com>
2025-06-12 23:29:39 +0900

    Initial commit

---
+++ src/main/java/com/example/hospin/HospinApplication.java
@@ -0,0 +1,13 @@
+package com.example.hospin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HospinApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HospinApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/hospin/HospinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hospin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HospinApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
